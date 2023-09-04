0ba6fe798b95534158d8fbf69a314bb0bd978ebf
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2023-09-04 09:57:36 +0200

    Add quantifiers to syntax

--- src/main/scala/definitions/Formula.scala
+++ src/main/scala/definitions/Formula.scala
@@ -7,20 +7,25 @@ sealed trait Formula {
             case And(l, r) => s"($l ∧ $r)"
             case Or(l, r) => s"($l ∨ $r)"
             case Not(l) => s"¬$l"
+            case Forall(v, inner) => s"⋀$v. $inner"
+            case Exists(v, inner) => s"⋁$v. $inner"
         
     def toLatex: String =
-        this match {
+        this match
             case Variable(name) => name
             case And(l, r) => s"(${l.toLatex} \\land ${r.toLatex})" 
             case Or(l, r) => s"(${l.toLatex} \\lor ${r.toLatex})" 
             case Not(l) => s"\\neg${l.toLatex}" 
-        }
+            case Forall(v, inner) => s"⋀${v.toLatex}. ${inner.toLatex}"
+            case Exists(v, inner) => s"⋁${v.toLatex}. ${inner.toLatex}"
 }
 
 case class Variable(name: String) extends Formula
 case class And(l: Formula, r: Formula) extends Formula
 case class Or(l: Formula, r: Formula) extends Formula
 case class Not(l: Formula) extends Formula
+case class Forall(v: Variable, inner: Formula) extends Formula
+case class Exists(v: Variable, inner: Formula) extends Formula
 
 extension (f1: Formula) {
     def unary_! = Not(f1)
